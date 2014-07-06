fba7c557c6677d79cfda98c2651e1bbd0ce586ad
Vladimir <glazachev.vladimir@gmail.com>
2014-07-06 18:52:38 +0400

    flood fill algo impl

--- src/matrixField.cpp
+++ src/matrixField.cpp
@@ -31,8 +31,44 @@ void MatrixField::floodFill(int value, int x, int y) {
 		return;
 	}
 
-	FloodFillStack stack(mWidth);
+	int x1;
 
+	bool spanTop = false;
+	bool spanBot = false;
+
+	FloodFillStack stack(mWidth);
+	stack.push(x, y);
+
+	while (stack.pop(x, y)) {
+		x1 = x;
+		while (x1 >= 0 && at(x1, y) == oldValue) {
+			x1--;
+		}
+		x1++;
+
+		spanTop = false;
+		spanBot = false;
+
+		while (x1 < mWidth && at(x1, y) == oldValue) {
+			set(value, x1, y);
+
+			if (!spanTop && y > 0 && at(x1, y - 1) == oldValue) {
+				stack.push(x1, y - 1);
+				spanTop = true;
+			} else if (spanTop && y > 0 && at(x1, y - 1) != oldValue) {
+				spanTop = false;
+			}
+
+			if (!spanBot && y < mHeight - 1 && at(x1, y + 1) == oldValue) {
+				stack.push(x1, y + 1);
+				spanBot = true;
+			} else if (spanBot && y < mHeight - 1 && at(x1, y + 1) != oldValue) {
+				spanBot = false;
+			}
+
+			x1++;
+		}
+	}
 }
 
 
